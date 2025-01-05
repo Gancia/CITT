3c8c7de77a71a906f863c3693a16f9527e3daff1
Gancia <105257523+Gancia@users.noreply.github.com>
2025-01-04 19:06:02 -0500

    css y js

--- djangoproject/sitioweb/urls.py
+++ djangoproject/sitioweb/urls.py
@@ -1,9 +1,12 @@
-
 from django.urls import path
 from . import views
 
 
 urlpatterns = [
-    path('', views.hello),
-    path('about/',views.about)
+    path('', views.hello, name='home'),  # Página inicial
+    path('about/', views.about, name='about'),  # Página "Acerca de"
+    path('quienes-somos/', views.quienes_somos, name='quienes_somos'),  # Página de Quienes Somos
+    path('laboratorios/', views.laboratorios, name='laboratorios'),  # Página de Laboratorios
+    path('investigaciones/', views.investigaciones, name='investigaciones'),  # Página de Investigaciones
+    # Agrega más rutas según necesites
 ]
