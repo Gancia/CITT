561ffedf089a8f57e6634fd4bd4e777612f48cee
John Cortés Pozo <stevencortespozo@gmail.com>
2025-01-11 22:13:02 -0500

    Se corrige error para continuar con el deploy

--- citt/settings.py
+++ citt/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'citt.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default='posrgres://postgres:postgres@localhost/postgres')
+    'default': dj_database_url.config(default='postrgres://postgres:postgres@localhost/postgres')
 }
 
 
