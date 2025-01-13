04e4a7cb1da3ff533ea57dba20e143c111b52b56
John Cortés Pozo <stevencortespozo@gmail.com>
2025-01-13 12:09:17 -0500

    Se agrega la funcionalidad de la interacción del submenu

--- webcitt/static/js/scripts.js
+++ webcitt/static/js/scripts.js
@@ -1,26 +1,41 @@
-// Ejemplo de extensión correcta en scripts.js
 $(document).ready(function() {
-  // Agregar funcionalidad después que dore.script.js se ejecute
-  $(window).on('doreInitComplete', function() {
-    // Asegurarse que el menú principal tenga la clase correcta
-    $("#app-container").addClass("menu-default");
+    // Función para mostrar el submenú correspondiente
+    function showSubMenu(link) {
+        // Oculta todos los submenús
+        $('.sub-menu ul').hide();
+        // Muestra el submenú correspondiente al link
+        $('.sub-menu ul[data-link="' + link + '"]').show();
+    }
 
-    // Manejar clicks en el menú principal
-    $(".main-menu ul li a").on("click", function() {
-      var link = $(this).data("link");
-      
-      // Activar el ítem del menú
-      $(".main-menu ul li").removeClass("active");
-      $(this).parent().addClass("active");
-
-      // Mostrar el submenú correspondiente
-      $(".sub-menu ul").hide();
-      $('.sub-menu ul[data-link="' + link + '"]').show();
-
-      // Asegurar que el submenú esté visible
-      if($("#app-container").hasClass("menu-sub-hidden")) {
-        $("#app-container").removeClass("menu-sub-hidden");
-      }
+    // Click handler para los items del menú principal
+    $('.main-menu ul li a').on('click', function(e) {
+        e.preventDefault();
+        
+        // Obtiene el data-link del item seleccionado
+        var link = $(this).attr('data-link');
+        
+        // Remueve la clase active de todos los items
+        $('.main-menu ul li').removeClass('active');
+        // Agrega la clase active al item seleccionado
+        $(this).parent('li').addClass('active');
+        
+        // Cambia las clases del contenedor principal
+        if ($('#app-container').hasClass('menu-sub-hidden')) {
+            $('#app-container').removeClass('menu-sub-hidden');
+        }
+        
+        // Muestra el submenú correspondiente
+        showSubMenu(link);
+        
+        // Si estamos en móvil, muestra temporalmente el submenú
+        if ($('#app-container').hasClass('menu-mobile')) {
+            $('#app-container').addClass('sub-show-temporary');
+        }
     });
-  });
+
+    // Inicialización - muestra el submenú de la sección activa
+    var activeLink = $('.main-menu ul li.active a').attr('data-link');
+    if (activeLink) {
+        showSubMenu(activeLink);
+    }
 });
--- webcitt/views.py
+++ webcitt/views.py
@@ -2,23 +2,34 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_view(request):
-    return render(request, "inicio.html")
+    return render(request, "inicio.html",
+                  {"active_section": "home"})
 
 def ott_view(request):
-    return render(request, 'ott.html')
+    return render(request, 'ott.html', {
+        'seccion_activa': 'ott'
+    })
 
 # Vista para el apartado Incubadora
 def incubadora_view(request):
-    return render(request, 'incubadora.html')
+    return render(request, 'incubadora.html', {
+        'seccion_activa': 'incubadora'
+    })
 
 # Vista para el apartado Unidades Experimentales
 def unidadesexperimentales_view(request):
-    return render(request, 'uexperimentales.html')
+    return render(request, 'uexperimentales.html', {
+        'seccion_activa': 'uexperimentales'
+    })
 
 # Vista para el apartado HubLab
 def hublab_view(request):
-    return render(request, 'hublab.html')
+    return render(request, 'hublab.html', {
+        'seccion_activa': 'hublab'
+    })
 
 # Vista para el apartado Formación
 def formacion_view(request):
-    return render(request, 'formacion.html')
+    return render(request, 'formacion.html', {
+        'seccion_activa': 'formacion'
+    })
