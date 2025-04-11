3ff07cbc5d3465d6173c2ed0320ff759b514f05d
Gancia <105257523+Gancia@users.noreply.github.com>
2025-04-11 11:40:48 -0500

    Revert "keep showing sub}bmenu and menu  in smaller displays"
    This reverts commit 9e48532a6c5bddd805e28f183399f452ae135f31.

--- webcitt/static/js/scripts.js
+++ webcitt/static/js/scripts.js
@@ -157,19 +157,10 @@ $(document).ready(function() {
 
     // Función para cambiar las clases del contenedor basado en el tamaño de la pantalla
     function toggleMenuClass() {
-        const container = $('#app-container');
         if (window.innerWidth <= 767) {
-            container.removeClass('menu-default').removeClass('menu-sub-hidden').removeClass('menu-hidden');
-            $('.sub-menu ul').show(); // Asegura que el submenú sea visible
+            $('#app-container').removeClass('menu-default').addClass('menu-hidden');
         } else {
-            container.removeClass('menu-hidden').addClass('menu-default');
-        }
-
-        // Asegura que el submenú sea visible si hay un menú activo
-        const activeLink = sessionStorage.getItem('activeMenu');
-        if (activeLink && ['ott', 'incubadora', 'uexperimentales', 'formacion'].includes(activeLink)) {
-            container.removeClass('menu-sub-hidden');
-            $('.sub-menu ul[data-link="' + activeLink + '"]').show();
+            $('#app-container').removeClass('menu-hidden').addClass('menu-default');
         }
     }
 
